aa155428f0bc4d628df2ca1db6845a9ec0493f38
LTBK23 <lalka@dtu.dk>
2025-02-10 13:25:22 +0100

    added answers within week00answers

--- week00_prep_answers.py
+++ week00_prep_answers.py
@@ -32,11 +32,25 @@ squares = square_list(list)
 print(squares)
 # %%
 # Exercice 4 While loop
-def fibonacci_stop(max):
+def fibonacci_stop(max_value):
     fibonacci_sequence = [1, 1]
-    while fibonacci_sequence[] < max:
+    while fibonacci_sequence[-1] + fibonacci_sequence[-2] < max_value:
         fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])
+        #print(fibonacci_sequence)
     return fibonacci_sequence
 
 print(fibonacci_stop(30))
+# %%
+# Exercise 5 Logical operations
+def clean_pitch(pitch, status):
+    # check for each pair of pitch and status if status is above 0 and pitch is not between 0 and 90 and correct pitch to -999
+    for i in range(len(pitch)):
+        if status[i] > 0 and (pitch[i] < 0 or pitch[i] > 90):
+            pitch[i] = -999
+    return pitch
+
+pitch = [-1, 2, 6, 95]
+status = [1, 0, 0, 0]  
+print(clean_pitch(pitch, status))
+
 # %%
